f11fc46ab32912d3a3c23f7d5869f5308f65e982
Hristo Traykov <hristotraykov98@gmail.com>
2023-10-03 01:54:09 +0300

    Updated libraries

--- Real_Time_Clock/Real_Time_Clock_Compile/Real_Time_Clock/No_GPS_Addon.ino
+++
@@ -1,5 +0,0 @@
-
-// ---------------------------------------------------- Update time from GPS --------------------------------------------------- //
-void UpdateTimeFromGPS(TinyGPSDate &d, TinyGPSTime &t) {
-  hasGPS = false;
-}
