e084ec19da26c19f1e93df5133b482e96528bf22
Hristo Traykov <hristotraykov98@gmail.com>
2024-12-29 12:51:03 +0200

    Updated web request handlers

--- Real_Time_Clock/Web_Request_Handlers.ino
+++ Real_Time_Clock/Web_Request_Handlers.ino
@@ -1,141 +1,151 @@
 
 // _____________________________________________ Web interface handling functions _____________________________________________ //
-void HandleWebInterface() { // Handler for the main interface
-  if (server.arg("timeSyncMode") == "auto") {
-    if (server.arg("setTimeWith") == "gps") {
-      timeUpdatePending = true;
-      
-      if (hasGPS) {
-        GPS_connectionTriesLeft = 30;
-        gpsSerial.begin(GPS_BAUD_RATE);
-      }
+void handleWebInterface() { // Handler for the main interface
+  if (server.hasArg("confirmResponse")) {
+    awaiting_confirmation = false;
+    response_message = ""; // Global variable
+#ifdef  RTC_INFO_MESSAGES
+    Serial.println(F("Response confirmed"));
+#endif
+  }
+  else if (server.hasArg("resentRequest")) {
+#ifdef  RTC_INFO_MESSAGES
+    Serial.println(F("Message resend requested"));
+#endif
+    sendWebpageResponse(response_message.c_str());
+  }
+  else if (server.arg("timeSyncMode") == "wifi") {
+    String ssid = server.arg("ssid");
 
-      SendWebpageResponse("Часовникът ще се свери чрез GPS");
-      overrideSettings = true;
+    if (WiFi.status() == WL_CONNECTED) {
+      if (ssid == WiFi.SSID()) {
+        sendWebpageResponse(("Часовникът вече е свързан с мрежа " + ssid + " и се свери през Интернет").c_str());
+        time_update_pending = true;
+      }
+      else {
+        sendWebpageResponse(("Часовникът беше сверен през Интернет. Настоящата мрежа е " + WiFi.SSID()).c_str());
+        time_update_pending = true;
+      }
     }
-    else if (server.arg("setTimeWith") == "wifi") {
-      int ssidValueLen = (server.arg("ssid")).length() + 1; // Store input ssid length
-      char ssid[ssidValueLen];
-      (server.arg("ssid")).toCharArray(ssid, ssidValueLen);
-      
-      if (WiFi.status() == WL_CONNECTED && (String)ssid == WiFi.SSID()) {
-        SendWebpageResponse(("Часовникът вече е свързан към " + (String)ssid).c_str());
-        UpdateTimeFromNTP();
-      }
-      else if ((String)ssid != "" && (server.arg("pass")).length() + 1 > 7) { // Check if the input is valid
-        int passValueLen = (server.arg("pass")).length() + 1; // Store input password length
-  
-        // Make SSID and password char arrays and fill them with the input values
-        char pass[passValueLen];
-        (server.arg("pass")).toCharArray(pass, passValueLen);
-  
-        int numberOfNetworks = WiFi.scanNetworks(); // Scan networks in area
-        bool isNetworkInRange = false;
-        
-        for (int i = 0; i < numberOfNetworks; i++) {
-          Serial.print(F("Network request: "));
-          Serial.print(ssid);
-          Serial.print(F(" | Network in range: "));
-          Serial.println(WiFi.SSID(i));
-          
-          if ((String)ssid == WiFi.SSID(i)) {
-            isNetworkInRange = true;
-            WiFi.begin(ssid, pass); // Check if the user's network is in range and try connecting to it
-            
-            for (int i = 0; i < CONNECT_TO_NETWORK_LOOP_COUNT; i++) {
-              if (WiFi.status() == WL_CONNECTED) {
-                SendWebpageResponse(("Часовникът се свърза с " + (String)ssid + ".\nIP: " + WiFi.localIP().toString()).c_str());
-                timeUpdatePending = true;
-                break;
-              }
-              
-              if (i == CONNECT_TO_NETWORK_LOOP_COUNT - 1) {
-                SendWebpageResponse("Времето за опит за свързване изтече. Проверете името и паролата");
-                break;
-              }
-              
-              delay(CONNECT_TO_NETWORK_LOOP_DELAY);
-            }
-          }
-        }
-        
-        if (!isNetworkInRange)
-          SendWebpageResponse(("The network " + (String)ssid + " is not in range").c_str());
-      }
-      else if (WiFi.status() == WL_CONNECTED) {
-        SendWebpageResponse("Промените са запазени и времето е сверено по Интернет");
-        UpdateTimeFromNTP();
-      }
-      else
-        SendWebpageResponse("Промените са запазени");
-      
-      GPS_connectionTriesLeft = 0;
-      overrideSettings = true;
+    else if (ssid != "" && (server.arg("pass")).length() + 1 > 7) {
+      validateNetworkInput(server.arg("pass"), ssid);
+    }
+#ifdef  GPS_MODULE
+    else if (set_time_with_gps)
+      sendWebpageResponse("Промените са запазени!");
+#endif
+    else
+      sendWebpageResponse("Моля въведете име и парола на мрежата!");
+
+#ifdef  GPS_MODULE
+    gps_connect_attempts_left = 0;
+#endif
+    editClockSettings("wifi", 1);
+  }
+#ifdef  GPS_MODULE
+  else if (server.arg("timeSyncMode") == "gps") {
+    activateGPS(); // GPS module function
+    sendWebpageResponse("Часовникът ще се свери чрез GPS");
+    editClockSettings("gps", 1);
+    time_update_pending = true;
+  }
+#endif
+  else if (server.arg("timeSyncMode") == "js") { // If the user opened the Web UI, update the time if needed
+    if (WiFi.status() != WL_CONNECTED) {
+      manualTimeUpdate();
+      sendWebpageResponse("Часовникът се свери автоматично от устройството Ви");
+    }
+    else {
+      sendWebpageResponse(("Часовникът беше сверен през Интернет. Настоящата мрежа е " + WiFi.SSID()).c_str());
+      time_update_pending = true;
+    }
+  }
+  else if (server.hasArg("daylightSaving")) {
+    editClockSettings(server.arg("daylightSaving").c_str(), 0);
+    sendWebpageResponse("Промените са запазени");
+  }
+  else if (server.hasArg("autoBrightnessControl")) {
+    if (server.arg("autoBrightnessControl") == "false") {
+      editClockSettings(server.arg("manualBrightnessLevel").c_str(), 3);
+      display_brightness = server.arg("manualBrightnessLevel").toInt();
     }
     else
-      SendWebpageResponse("Промените са запазени");
-  }
-  else if (server.arg("timeSyncMode") == "manual") { // If the user used the manual time set option - set the new time
-    int manualTime = (server.arg("manualTimeValue")).toInt();
-    rtc.adjust(DateTime(rtc.now().year(), rtc.now().month(), rtc.now().day(), manualTime / 100, manualTime % 100, 0));
-    overrideSettings = true;
+      display_brightness = DEFAULT_BRIGHTNESS;
 
-    SendWebpageResponse("Промените са запазени и времето е ръчно настроено");
+    editClockSettings(server.arg("autoBrightnessControl").c_str(), 2);
+    sendWebpageResponse("Промените са запазени");
   }
-  else
-    server.send(200, "text/html", webpageSetup); // Show main page at the begining
+  else {
+    streamFileToServer("/index.html", "text/html"); // Show main page at the begining
+  }
 
-  if (overrideSettings) {
-    Serial.print(F("Daylight saving is "));
-    Serial.println(server.arg("daylightSaving"));
-    Serial.print(F("Control brightness "));
-    Serial.println(server.arg("brightnessControl"));
-    Serial.print(F("Brightness value "));
-    Serial.println(server.arg("brightnessValue"));
-    Serial.print(F("Time sync mode: "));
-    Serial.println(server.arg("timeSyncMode"));
-    Serial.print(F("Time sync type: "));
-    Serial.println(server.arg("setTimeWith"));
-    
-    EditClockSettings((server.arg("daylightSaving") == "ON"), (server.arg("setTimeWith") == "gps"), (server.arg("brightnessControl") == "auto"),
-                       server.arg("brightnessValue").toInt(), "2", (WiFi.localIP().toString()).c_str()); // Edit tz!
-    overrideSettings = false;
-    GetClockSettings();
+  if (server.hasArg("timezoneHoursOffset"))
+    editClockSettings(server.arg("timezoneHoursOffset").c_str(), 4);
+
+#ifdef  RTC_INFO_MESSAGES
+  uint8_t argCount = server.args();
+
+  for (uint8_t i = 0; i < argCount; i++) {
+    Serial.print(server.argName(i));
+    Serial.print(F(" = "));
+    Serial.println(server.arg(i));
   }
+#endif
 }
 
-void SendIP() { // Handler for IP address page
-  server.send(200, "text/plain", WiFi.localIP().toString());
+void sendIP() { // Handler for IP address page
+  server.send(200, "text/plain", WiFi.localIP().toString().c_str());
+#ifdef  RTC_INFO_MESSAGES
   Serial.print(F("IP: "));
   Serial.println(WiFi.localIP());
+#endif
 }
 
-void SendLockImage() { // Handler for lock image
-  File dataFile = LittleFS.open("/Lock.png", "r");
-  server.streamFile(dataFile, "image/png");
-  dataFile.close();
-}
-
-void SendLogoIcon() { // Handler for browser tab icon
-  File dataFile = LittleFS.open("/neonLogoIcon.ico", "r");
-  server.streamFile(dataFile, "image/x-icon");
-  dataFile.close();
-}
-
-void SendWebpageResponse(const char* webpageResponse) {
-  server.send(200, "text/plain", webpageResponse);
+void sendWebpageResponse(const char *webpage_response) { // Send info to the server
+  server.send(200, "text/plain", webpage_response);
   server.handleClient();
-  Serial.println(webpageResponse);
+
+#ifdef  RTC_INFO_MESSAGES
+  Serial.println(webpage_response);
+#endif
+
+  if (!awaiting_confirmation) {
+    awaiting_confirmation = true;
+    response_message = webpage_response;
+  }
 }
 
-void SendScript() { // Handler for javascript
-  server.send(200, "text/javascript", mainScript);
+void streamFileToServer(const char *filename, const char *filestream_format) {
+  File data_file = LittleFS.open(filename, "r");
+
+  server.streamFile(data_file, filestream_format);
+  data_file.close();
 }
 
-void SendSettings() { // Handler for user personalizations (xml settings)
-  server.send(200, "text/xml", settingsFile);
-}
+void validateNetworkInput(const String& pass, const String& ssid) {
+  uint8_t number_of_networks = WiFi.scanNetworks(); // Scan networks in area
+  bool is_network_in_range = false;
 
-void SendStyling() { // Handler for styling
-  server.send(200, "text/css", mainStyling);
+  for (uint8_t i = 0; i < number_of_networks; i++) {
+#ifdef  RTC_INFO_MESSAGES
+    Serial.print(F("Network request: "));
+    Serial.print(ssid);
+    Serial.print(F(" | Network in range: "));
+    Serial.println(WiFi.SSID(i));
+#endif
+
+    if (ssid == WiFi.SSID(i)) {
+      is_network_in_range = true;
+
+      if (connectClockToNetwork(ssid, pass)) {
+        sendWebpageResponse(("Часовникът се свърза с мрежа " + ssid + ".\nIP: " + WiFi.localIP().toString()).c_str());
+        break;
+      }
+      else
+        sendWebpageResponse("Времето за опит за свързване изтече. Проверете името и паролата.");
+    }
+  }
+
+  if (!is_network_in_range)
+    sendWebpageResponse(("Мрежата " + ssid + " не е в обхват").c_str());
 }
