ccda4b16cd895ab6c098470efc14effb120e7552
Hristo Traykov <hristotraykov98@gmail.com>
2024-12-29 12:39:00 +0200

    Updated Light Sensitivity Addon

--- Real_Time_Clock/Light_Sensitivity_Addon.ino
+++ Real_Time_Clock/Light_Sensitivity_Addon.ino
@@ -1,19 +1,29 @@
 
+// _________________________________________________ Light sensitivity module functions _______________________________________________ //
+
+#ifdef  LIGHT_SENSITIVITY_MODULE
+int light_sensor_value;
+
 // ------------------------------------- Change the brightness of the clock depending on the light ------------------------------------ //
-void AutoBrightness() {
-  if (hasLightSensor && autoBrightness && flashesOnConnect == 0) {
-    lastDisplayBrightness = displayBrightness;
-    lightSensorValue = analogRead(0); // Read from the light sensor
-    Serial.print(F("Light sensor value: "));
-    Serial.print(lightSensorValue);
-    Serial.print(F(", "));
-    
-    if (lightSensorValue <= 50)
-      displayBrightness = 0;
-    else if (lightSensorValue > 50 && lightSensorValue <= 600)
-      displayBrightness = 2;
+void autoSetBrightness() {
+  if (auto_brightness && blink_count == 0) {
+    last_display_brightness = display_brightness;
+    light_sensor_value = analogRead(0); // Read from the light sensor
+
+    if (light_sensor_value <= 50)
+      display_brightness = 0;
+    else if (light_sensor_value > 50 && light_sensor_value <= 500)
+      display_brightness = 2;
+    else if (light_sensor_value > 500 && light_sensor_value <= 750)
+      display_brightness = 4;
     else
-      displayBrightness = 4;
+      display_brightness = 6;
+
+#ifdef  RTC_INFO_MESSAGES
+    Serial.print(F("Light sensor value: "));
+    Serial.print(light_sensor_value);
+    Serial.print(F(", "));
+#endif
   }
-  FlashDisplay();
 }
+#endif
